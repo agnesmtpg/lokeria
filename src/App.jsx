172b161a640ec043c2a43cc2ceb90ba19ff74947
agneskatrin182 <username@users.noreply.github.com>
2025-08-18 07:49:10 +0800

    tugas

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import LoginRegister from './pages/LoginRegister'
+import Vacancy from './pages/Vaccany'
+import JobDetail from './pages/JobDetail'
+import Dashboard from './pages/Dashboard'
+import ListJobTable from './components/ListJobTable'
+import AddNewJob from './components/AddNewJob'
+import CardProfile from './components/CardProfile'
+import { AuthProvider } from './context/AuthContext'
+import { LoginRoute, PrivateRoute } from './routes/ProtectedRoute'
+import WelcomeDashboard from './components/WelcomeDashboard'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <AuthProvider>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Navbar />
+              <Home />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <LoginRoute>
+              <LoginRegister mode="login" />
+            </LoginRoute>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <LoginRoute>
+              <LoginRegister mode="register" />
+            </LoginRoute>
+          }
+        />
+        <Route
+          path="/Vacancy"
+          element={
+            <>
+              <Navbar />
+              <Vacancy />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/jobs/:id"
+          element={
+            <>
+              <Navbar />
+              <JobDetail />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        >
+          <Route index element={<WelcomeDashboard />} />
+          <Route path="list" element={<ListJobTable />} />
+          <Route path="add" element={<AddNewJob />} />
+          <Route path="edit/:id" element={<AddNewJob />} />
+          <Route path="profile" element={<CardProfile />} />
+        </Route>
+      </Routes>
+    </AuthProvider>
   )
 }
-
-export default App
